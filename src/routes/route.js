580deba93aca87182272384a0f98d833d300d337
Amitkm997 <amitkm997@gmail.com>
2022-09-14 18:24:18 +0530

    this is the only dummy commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,16 +3,14 @@ const router = express.Router();
 const collegeControler=require('../controller/collegeController')
 const internController=require('../controller/internController')
 
+
+
+//this is the project about internship
+
 router.post('/functionup/colleges',collegeControler.createCollege)
 
 router.post('/functionup/interns',internController.createIntern)
 
 router.get('/functionup/collegeDetails',internController.getIntership)
-
-
-
-
-
-
 
 module.exports = router;
