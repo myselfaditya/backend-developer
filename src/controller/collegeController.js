91d9b3a7eb133363631298a1726b6fb7313f965f
Amitkm997 <amitkm997@gmail.com>
2022-09-15 16:44:48 +0530

    this is the updated code

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -8,14 +8,20 @@ const isValid =function(value){
 }
 
 const createCollege=async function(req,res){
-    data = req.body;
+    try{
+        data = req.body;
 
     //checking for the empty data
     if((Object.keys(data).length == 0) ) {return res.status(400).send({status:false,messege:"please provide some values"})}
 
     //cheking the credentials to be true
     let{name,fullName,logoLink}=data
-    if(!isValid(name)) return res.status(400).send({status:false,messege:"please provide name"})
+    
+    
+    if(!isValid(name)) return res.status(400).send({status:false,messege:"Please provide name"})
+    let uniqueName = await collegeModel.findOne({ name })
+        if (uniqueName) { return res.status(400).send({ status: false, messege: "This College already exist" }) }
+
     if(!isValid(fullName)) return res.status(400).send({status:false,messege:"please provide fullName"})
     if(!isValid(logoLink)) return res.status(400).send({status:false,messege:"please provide logoLink"})
     
@@ -25,6 +31,9 @@ const createCollege=async function(req,res){
 
     let saveCollege=await collegeModel.create(data);
     return res.status(201).send({status:true,data:saveCollege});
+}catch(error){
+    return res.status(500).send({status:false,messege:error.messege})
+}
 }
 
 module.exports.createCollege=createCollege;
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -7,19 +7,26 @@ const mongoose = require('mongoose')
 const createIntern = async function (req, res) {
     try {
         data = req.body
-        let { name, mobile, email, collegeId } = data
+        let { name, mobile, email, collegeName,isDeleted} = data
 
-        var isValidData = function (data) {
-            return Object.keys(data).length > 0;
-        }
+        // var isValidData = function (data) {
+        //     return Object.keys(data).length > 0;
+        // }
 
-        if (!name) { return res.status(400).send({ status: false, msg: "College name is mandatory" }) }
+        if (!name) { return res.status(400).send({ status: false, msg: " intern name is mandatory" }) }
 
         if (!mobile) { return res.status(400).send({ status: false, msg: "mobile no. is mandatory" }) }
 
         if (!email) { return res.status(400).send({ status: false, msg: "email is mandatory" }) }
 
-        if (!collegeId) { return res.status(400).send({ status: false, msg: "collegeID is mandatory" }) }
+        
+        // if (!collegeName) return res.status(400).send({ status: false, messege: "please provide college Name" })
+        // const saveData = await collegeModel.findOne({ name: collegeName });
+        
+        //   collegeId=saveData._id
+
+
+        
 
         //-------mobile validation-------------//
         let isValidMobile = function (mobile) { return /^(\()?\d{3}(\))?(|\s)?\d{3}(|\s)\d{4}$/.test(mobile) }
@@ -29,61 +36,68 @@ const createIntern = async function (req, res) {
         if (uniqueMobile) { return res.status(400).send({ status: false, msg: "this number already exist" }) }
 
         //-----------email validation----------------//
-        
+
         const validEmail = function (email) { return /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email) }
-        if (!validEmail(email)) { return res.status(401).send({ status: false, msg: "provide email in correct format e.g. xyz@abc.com " }) }
+        if (!validEmail(email)) { return res.status(400).send({ status: false, msg: "provide email in correct format e.g. xyz@abc.com " }) }
 
         const uniqueEmail = await internModel.findOne({ email })
         if (uniqueEmail) {
-            return res.status(401).send({ staus: false, msg: "email already exist " })
+            return res.status(400).send({ staus: false, msg: "email already exist " })
         }
 
-        if (collegeId) {
-            if (!mongoose.isValidObjectId(collegeId)) { return res.status(400).send({ status: false, msg: "collegeId is not in format" }) }
-            else {
-                if (!await collegeModel.findById(collegeId)) {
-                    return res.status(400).send({ status: false, msg: "collegeId is not valid" })
-                }
-            }
-        }
+        // if (collegeId) {
+        //     if (!mongoose.isValidObjectId(collegeId)) { return res.status(400).send({ status: false, msg: "collegeId is not in format" }) }
+        //     else {
+        //         if (!await collegeModel.findById(collegeId)) {
+        //             return res.status(400).send({ status: false, msg: "collegeId is not valid" })
+        //         }
+        //     }
+        // }
+        let iscollegeId = await collegeModel.findOne({ name:collegeName }).select({ _id: 1 });
+    if (!iscollegeId) {
+      return res.status(400).send({ status: false, message: "college name is not exist" });
+    }
+    let id = iscollegeId._id.toString()
+    data.collegeId = id
+    if (isDeleted == true) {
+      res.status(400).send({ status: false, message: "Cannot input isDeleted as true while registering" });
+      return;
+    }
 
         const createData = await internModel.create(data)
-        return res.status(201).send({ status: true, msg: "created data", data: createData });
+        return res.status(201).send({ status: true, data: createData });
 
     } catch (err) {
         res.status(500).send({ status: false, msg: err.message })
     }
 }
 //========================getDetails===================================
-const getIntership=async function(req,res){
-    const collegeName=req.query.name
-    const saveData=await collegeModel.findOne({name:collegeName});
-    
-    if(!saveData) res.status(400).send({status:false,messege:"No college appears with this college name"});
-    // let collegeId=saveData[0]._id.toString();
-    // console.log(collegeId);
-    const interns=await internModel.find({collegeId:saveData._id}).select({name:1,email:1,mobile:1})
-    // const data=await internModel.find(data._id);
-    const data = {
-        name: saveData.name,
-        fullName: saveData.fullName,
-        logoLink: saveData.logoLink,
-        interns: interns.length==0 ? "No such Intern" : interns
-      };
-    return res.status(200).send({status:true,data:data});
+const getIntership = async function (req, res) {
+    try {
+        const collegeName = req.query.collegeName
+        if (!collegeName) return res.status(400).send({ status: false, messege: "please provide college Name" })
+        const saveData = await collegeModel.findOne({ name: collegeName });
+
+        if (!saveData) return res.status(400).send({ status: false, messege: "No college appears with this college name" });
+        // let collegeId=saveData[0]._id.toString();
+        // console.log(collegeId);
+        const saveInterns = await internModel.find({ collegeId: saveData._id }).select({ name: 1, email: 1, mobile: 1 })
+        if (saveInterns.length == 0) {
+            return res.status(400).send({ status: false, message: "No interns data availabe in college" })
+        }
+        // const data=await internModel.find(data._id);
+        const data = {
+            name: saveData.name,
+            fullName: saveData.fullName,
+            logoLink: saveData.logoLink,
+            //interns: interns.length==0 ? return res.status(400).send({status:false,messege:"No such Intern exist"}) : interns
+            interns: saveInterns
+        }
+        return res.status(200).send({data: data });
+    } catch (error) {
+        res.status(500).send({ status: false, messege: error.message })
+    }
 }
-
-//const regex= 
-// const isValid = function(value){
-//     if(typeof value === 'undefined' || value === null) 
-//     {return false }
-//     if(typeof value === "string" && value.trim().length===0){
-//         return false
-//     }
-//     return true;
-// }
-
-
 
 module.exports = { createIntern }
 module.exports.getIntership = getIntership
